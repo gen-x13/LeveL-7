2d961bd81d00fe3eced0d57fbd5aa21cb030fd9c
genixcode <94076315+gen-x13@users.noreply.github.com>
2026-07-02 11:27:56 +0400

    Update webapp.py

--- Level 7 - GB/webapp.py
+++ Level 7 - GB/webapp.py
@@ -452,7 +452,7 @@ elif selected == "Early Warning":
                     
                     results.append(results_dict)
                     
-                carousel(items=results, width=50)
+                carousel(items=results, width=50, height=200)
                     
             with left:
                 st.caption("🏗 It's still under construction, come back in a few days")
